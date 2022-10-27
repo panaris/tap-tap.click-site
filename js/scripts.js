555936a78721261285148bf25b63ebb3c3002272
Panagiotis Christoforakis <panagiotischristoforakis@gmail.com>
2022-10-27 06:05:45 +0300

    first fixes

--- js/scripts.js
+++ js/scripts.js
@@ -162,7 +162,7 @@
     if ($('.pagepiling').length > 0){
       	$('.pagepiling').pagepiling({
     		scrollingSpeed: 280,
-		    loopBottom:true,
+		    loopBottom:false,
 		    anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8'],
 		    afterLoad: function(anchorLink, index){
 	           navbarFullpage();
